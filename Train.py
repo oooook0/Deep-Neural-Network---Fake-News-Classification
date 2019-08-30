ae659b7cda912a00a00accab53c5aa6cb2df8787
oooook0 <yitao.sun@baruchmail.cuny.edu>
2019-08-29 22:04:27 -0400

    correct indentation issue

--- Train.py
+++ Train.py
@@ -11,8 +11,8 @@ import os
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_string('tensor_hub_model', 'https://tfhub.dev/google/nnlm-en-dim128/1',
-					"""	define the word embeding model to use
-					""")
+                    """ define the word embeding model to use
+                    """)
 
 tf.app.flags.DEFINE_integer('max_steps', 1000,
                             """Number of batches to run.""")
@@ -26,9 +26,9 @@ def load_data_set(file):
     """
     load the csv dataset into dataframe and break into train and test set (80% split)
     """
-	df = pd.read_csv(file)
-	msk = np.random.rand(len(df)) < 0.8
-	return df[msk], df[~msk]
+    df = pd.read_csv(file)
+    msk = np.random.rand(len(df)) < 0.8
+    return df[msk], df[~msk]
 
 def one_hot(dataset, match):
 
@@ -36,23 +36,23 @@ def one_hot(dataset, match):
     conducted one hot encoding for dependent variable(y)
     """
 
-	dataset['y'] = 0
-	dataset.loc[dataset[match] == 'FAKE', 'y'] = 1
-	return dataset
+    dataset['y'] = 0
+    dataset.loc[dataset[match] == 'FAKE', 'y'] = 1
+    return dataset
 
 def get_predictions(estimator, input_fn):
 
-	"""
-	using the model to output predictions 
-	"""
+    """
+    using the model to output predictions 
+    """
 
-  	return [x["class_ids"][0] for x in estimator.predict(input_fn=input_fn)]
+    return [x["class_ids"][0] for x in estimator.predict(input_fn=input_fn)]
 
 def serving_fn():
 
-	"""
-	tensorflow feature loading 
-	"""
+    """
+    tensorflow feature loading 
+    """
     receiver_tensor = {
         "text": tf.placeholder(dtype=tf.string, shape=None)
     }
@@ -65,85 +65,85 @@ def serving_fn():
     return tf.estimator.export.ServingInputReceiver(features, receiver_tensor)
 
 def main(argv=None):
-	################################# Data Loading #######################################
-	train, test = load_data_set("./data/fake_or_real_news.csv")
+    ################################# Data Loading #######################################
+    train, test = load_data_set("./data/fake_or_real_news.csv")
 
-	train = one_hot(train, 'label')
-	test = one_hot(test, 'label')
+    train = one_hot(train, 'label')
+    test = one_hot(test, 'label')
 
-	print(train.shape)
-	print(test.shape)
-	print(train.head())
+    print(train.shape)
+    print(test.shape)
+    print(train.head())
 
-	print('Fake news percentage is {}.'.format(len(train[train['y'] ==1])/len(train)))
-	
-	################################## Modeling ##########################################
+    print('Fake news percentage is {}.'.format(len(train[train['y'] ==1])/len(train)))
+    
+    ################################## Modeling ##########################################
 
-	embedded_text_feature_column = hub.text_embedding_column(
-	    key="text", 
-	    module_spec=FLAGS.tensor_hub_model,
-	    trainable=True)
+    embedded_text_feature_column = hub.text_embedding_column(
+        key="text", 
+        module_spec=FLAGS.tensor_hub_model,
+        trainable=True)
 
-	# Transform Data to TF data type
+    # Transform Data to TF data type
 
-	train_input_fn = tf.estimator.inputs.pandas_input_fn(
-		train, train['y'], num_epochs=None, shuffle=True)
+    train_input_fn = tf.estimator.inputs.pandas_input_fn(
+        train, train['y'], num_epochs=None, shuffle=True)
 
-	predict_train_input_fn = tf.estimator.inputs.pandas_input_fn(
-	    train, train['y'], shuffle=False)
+    predict_train_input_fn = tf.estimator.inputs.pandas_input_fn(
+        train, train['y'], shuffle=False)
 
-	predict_test_input_fn = tf.estimator.inputs.pandas_input_fn(
-	    test, test['y'], shuffle=False)
+    predict_test_input_fn = tf.estimator.inputs.pandas_input_fn(
+        test, test['y'], shuffle=False)
 
 
-	estimator = tf.estimator.DNNClassifier(
-	    hidden_units=[500, 100],
-	    feature_columns=[embedded_text_feature_column],
-	    n_classes=2,
-	    optimizer=tf.train.ProximalAdagradOptimizer(learning_rate=0.003, l2_regularization_strength=0.01),
-	    #model_dir=FLAGS.base_export_dir
-	    )
+    estimator = tf.estimator.DNNClassifier(
+        hidden_units=[500, 100],
+        feature_columns=[embedded_text_feature_column],
+        n_classes=2,
+        optimizer=tf.train.ProximalAdagradOptimizer(learning_rate=0.003, l2_regularization_strength=0.01),
+        #model_dir=FLAGS.base_export_dir
+        )
 
-	# Train model
+    # Train model
 
-	estimator.train(input_fn=train_input_fn, steps=FLAGS.max_steps)
+    estimator.train(input_fn=train_input_fn, steps=FLAGS.max_steps)
 
-	# Evalute model and get accuracy
+    # Evalute model and get accuracy
 
-	train_eval_result = estimator.evaluate(input_fn=predict_train_input_fn)
-	test_eval_result = estimator.evaluate(input_fn=predict_test_input_fn)
+    train_eval_result = estimator.evaluate(input_fn=predict_train_input_fn)
+    test_eval_result = estimator.evaluate(input_fn=predict_test_input_fn)
 
-	print("Training set accuracy: {accuracy}".format(**train_eval_result))
-	print("Test set accuracy: {accuracy}".format(**test_eval_result))
+    print("Training set accuracy: {accuracy}".format(**train_eval_result))
+    print("Test set accuracy: {accuracy}".format(**test_eval_result))
 
-	print(estimator.evaluate(input_fn=predict_test_input_fn)["accuracy_baseline"])
+    print(estimator.evaluate(input_fn=predict_test_input_fn)["accuracy_baseline"])
 
-	estimator.export_savedmodel(export_dir_base=FLAGS.base_export_dir, serving_input_receiver_fn=serving_fn)
-	
-	################################confusion matrix######################################
+    estimator.export_savedmodel(export_dir_base=FLAGS.base_export_dir, serving_input_receiver_fn=serving_fn)
+    
+    ################################confusion matrix######################################
 
-	LABELS = [
-	    "FAKE", "REAL"
-	]
+    LABELS = [
+        "FAKE", "REAL"
+    ]
 
-	# Create a confusion matrix on training data.
+    # Create a confusion matrix on training data.
 
-	with tf.Graph().as_default():
-	  cm = tf.confusion_matrix(train["y"], 
-	                           get_predictions(estimator, predict_train_input_fn))
-	  with tf.Session() as session:
-	    cm_out = session.run(cm)
+    with tf.Graph().as_default():
+      cm = tf.confusion_matrix(train["y"], 
+                               get_predictions(estimator, predict_train_input_fn))
+      with tf.Session() as session:
+        cm_out = session.run(cm)
 
-	# Normalize the confusion matrix so that each row sums to 1.
-	
-	cm_out = cm_out.astype(float) / cm_out.sum(axis=1)[:, np.newaxis]
+    # Normalize the confusion matrix so that each row sums to 1.
+    
+    cm_out = cm_out.astype(float) / cm_out.sum(axis=1)[:, np.newaxis]
 
-	sns.heatmap(cm_out, annot=True, xticklabels=LABELS, yticklabels=LABELS)
-	plt.xlabel("Predicted")
-	plt.ylabel("True")
-	plt.show()
+    sns.heatmap(cm_out, annot=True, xticklabels=LABELS, yticklabels=LABELS)
+    plt.xlabel("Predicted")
+    plt.ylabel("True")
+    plt.show()
 
 if __name__ == "__main__":
 
-	tf.logging.set_verbosity(tf.logging.INFO)
-	tf.app.run()
+    tf.logging.set_verbosity(tf.logging.INFO)
+    tf.app.run()
